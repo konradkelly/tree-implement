74015c7b52ec2a35b4d0c1cede13c4f1c0824d90
konradkelly <konradky@gmail.com>
2026-02-12 14:52:32 -0800

    refactor class constructor and implement preOrder, postOrder, and inOrder methods

--- src/Traversal.java
+++ src/Traversal.java
@@ -1,32 +1,42 @@
 public class Traversal {
   public static void main(String[] args) {
-    TreeNode root = new TreeNode(10);
-    //System.out.println(root.data);
+    TreeNode root = new TreeNode(10, null, null);
+    //System.out.println(root.value);
+    root.left = new TreeNode(9, null, null);
+    //System.out.println(root.left.value);
+    root.left.left = new TreeNode(5, null, null);
+    //System.out.println(root.left.left.value);
+    root.left.right = new TreeNode(2, null, null);
+    //System.out.println(root.left.right.value);
 
-    root.left = new TreeNode(9);
-    root.right = new TreeNode(15);
-    //System.out.println(root.right.data);
-    
-    root.left.left = new TreeNode(5);
-    root.left.right = new TreeNode(2);
+    root.right = new TreeNode(15, null, null);
+    root.right.left = new TreeNode(-3, null, null);
+    root.right.right = new TreeNode(5, null,null);
+    root.right.right.right = new TreeNode(22, null, null);
 
-    root.right = new TreeNode(15);
-    root.right.left = new TreeNode(-3);
-    root.right.right = new TreeNode(5);
-
-    root.right.right.right = new TreeNode(22);
-
-    preOrder(root);
+    //preOrder(root);
+    //postOrder(root);
+    inOrder(root);
   }
-  public static void preOrder(TreeNode current) {
-    //if null: return
-    if (current == null) return;
-    
-    //print data
-    System.out.println(current.data);
-    //search left
-    preOrder(current.left);
-    //search right
-    preOrder(current.right);
+  //Print a tree rooted at the given node in pre-order
+  public static void preOrder(TreeNode node) {
+    if (node == null) return;
+    System.out.println(node.value);
+    preOrder(node.left);
+    preOrder(node.right);
+  }
+  
+  public static void postOrder(TreeNode node) {
+    if (node == null) return;
+    postOrder(node.left);
+    postOrder(node.right);
+    System.out.println(node.value);
+  }
+
+  public static void inOrder(TreeNode node) {
+    if (node == null) return;
+    inOrder(node.left);
+    System.out.println(node.value);
+    inOrder(node.right);
   }
 }
--- src/TreeNode.java
+++ src/TreeNode.java
@@ -1,9 +1,11 @@
 public class TreeNode {
-    public int data;
+    public int value;
     public TreeNode left;
     public TreeNode right;
 
-    public TreeNode(int data) {
-        this.data = data;
-    }
+    public TreeNode(int value, TreeNode left, TreeNode right) {
+        this.value = value;
+        this.left = left;
+        this.right = right;
+    }    
 }
